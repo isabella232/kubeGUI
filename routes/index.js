8bb75d17e9b2c7ab83e466de5333dad8cf9fb326
kubeGUI <kubeGUI@users.noreply.github.com>
2016-05-03 14:47:51 +0200

    Added google request for testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET status page. */
 router.get('/status', function(req, res, next) {
-  res.end('status');
+  request('http://www.google.com', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Show the HTML for the Google homepage.
+    }
+  })
 });
 
 module.exports = router;

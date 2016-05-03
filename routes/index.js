081cb9e96ec5c8672fad18944bd90b41335db4f5
kubeGUI <kubeGUI@users.noreply.github.com>
2016-05-03 14:54:17 +0200

    Added request to API Server, almost working

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,28 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var host = process.env.KUBEGUIHOST || process.env.KUBERNETES_SERVICE_HOST;
+var port = process.env.KUBEGUIPORT || process.env.KUBERNETES_SERVICE_PORT || '443';
+var username = process.env.KUBEGUIUSERNAME;
+var password = process.env.KUBEGUIPASSWORD;
+var url = 'https://' + host + ':' + port + '/api/v1';
+
+var options = {
+  url: url,
+  auth: {
+    user: username,
+    password: password
+  }
+}
+
 /* GET status page. */
 router.get('/status', function(req, res, next) {
-  request('http://www.google.com', function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body) // Show the HTML for the Google homepage.
+  request(options, function(error, response, body) {
+    if (error) {
+      console.dir(error)
+      return
     }
+    console.dir(body)
   })
 });
 
